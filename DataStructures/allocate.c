6f29317ce9094a1512f018d60a4344ff172bca7b
saulvaldelvira <saulvaldelvira@gmail.com>
2022-12-31 14:49:52 +0100

    Allocate

---
+++ DataStructures/allocate.c
@@ -0,0 +1,38 @@
+#include "allocate.h"
+#include <stdlib.h>
+
+void* alloc_integer(int n){
+        int * ptr = malloc(sizeof(int));
+        *ptr = n;
+        return ptr;
+}
+
+void* alloc_char(char c){
+        char * ptr = malloc(sizeof(char));
+        *ptr = c;
+        return ptr;
+}
+
+void* alloc_float(float f){
+        float * ptr = malloc(sizeof(float));
+        *ptr = f;
+        return ptr;
+}
+
+void* alloc_double(double d){
+        double * ptr = malloc(sizeof(double));
+        *ptr = d;
+        return ptr;
+}
+
+void* alloc_long(long l){
+        long * ptr = malloc(sizeof(long));
+        *ptr = l;
+        return ptr;
+}
+
+void* alloc_long_long(long long ll){
+        long long * ptr = malloc(sizeof(long long));
+        *ptr = ll;
+        return ptr;
+}
---
+++ DataStructures/allocate.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef ALLOCATE_H
+#define ALLOCATE_H
+void* alloc_integer(int n);
+void* alloc_char(char c);
+void* alloc_float(float f);
+void* alloc_double(double d);
+void* alloc_long(long l);
+void* alloc_long_long(long long l);
+#endif
