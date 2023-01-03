709aa8774f0922fa5adfcaf588efe545e0291c7c
saulvaldelvira <saulvaldelvira@gmail.com>
2023-01-03 15:00:37 +0100

    Stack test

--- test/stack_test.c
+++ test/stack_test.c
@@ -4,7 +4,8 @@
 int main(){
         int n = 10000000;
         printf("[Starting Stack Test]\n Workload: %d\n", n);
-        double tmp = get_time(), total;
+        TIMESTAMP_START
+
         Stack stack = stack_init(Comparators.integer);
         stack_configure(&stack, FREE_ON_DELETE);
         assert(stack_isempty(stack));
@@ -18,13 +19,14 @@ int main(){
         for(int i=0; i<n; i++){
                 stack_push(&stack, alloc_int(i));
         }
+        assert(!stack_isempty(stack));
         for(int i=n-1; i>=0; i--){
                 assert(i == * (int*) stack_peek(stack));
                 assert(i == * (int*) stack_pop(&stack));
         }
         assert(stack_isempty(stack));
 
-        total = get_time() - tmp;
+        TIMESTAMP_STOP
 
-        printf("[Stack Test Finished in %.3f seconds]\n", total);
+        printf("[Stack Test Finished in %.3f seconds]\n\n", timestamp_diff);
 }
