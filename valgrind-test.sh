ab8bd6e603306948debedaf7275c6e6d544a9714
Saúl Valdelvira <saulvaldelvira@gmail.com>
2023-03-17 16:39:24 +0100

    Ignore standard error when running valgrind-test
    A lot of the tests often check if functions treat errors
    like they are suposed to.
    For example, they call functions with NULL parameters to check if
    they print an error message and return an error code.
    
    The problem is, when running valgrind, those messages are annoying,
    and get in the way of valgrind's own output.

--- valgrind-test.sh
+++ valgrind-test.sh
@@ -1,10 +1,13 @@
+#!/bin/bash
+if [ -d "./bin" ]; then
 status=0
 for t in ./bin/*
 do
-valgrind --leak-check=yes --error-exitcode=1 "$t"
+valgrind --leak-check=yes --error-exitcode=1  --log-fd=1 2>/dev/null "$t"
 status=$?
 if [ $status -ne 0 ] ;
 then
 exit $status
 fi
 done
+fi
