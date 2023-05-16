664924f8d7f66225892dca4b8f78ff1294fc52bb
Saúl Valdelvira <saul@saulv.es>
2023-05-16 13:20:32 +0200

    Small bug in heap test
    I used cast_int twice, so the first number was overwritten by
    the second (16), which exists in the vector, and causes the assertion to fail.

--- test/heap_test.c
+++ test/heap_test.c
@@ -13,7 +13,8 @@ void change_priority(){
 	int elements[] = {12 ,14, 15, 20, 16, 17, 19, 24, 30};
 	heap_add_array(min, elements, 9);
 
-	assert(heap_change_priority(min, cast_int(-78), cast_int(16)) != SUCCESS);
+	int tmp = -78;
+	assert(heap_change_priority(min, &tmp, cast_int(90)) != SUCCESS);
 	Ignore_Error(assert(heap_change_priority(min, NULL, NULL) != SUCCESS), 0);
 	Ignore_Error(assert(heap_change_priority(min, &elements[0], NULL) != SUCCESS), 24);
 
